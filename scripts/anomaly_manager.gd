7a69fef44e267fba3456cad02239be41b862295f
Owen <ojr4316@rit.edu>
2026-01-11 16:39:19 -0500

    anomaly win

--- scripts/anomaly_manager.gd
+++ scripts/anomaly_manager.gd
@@ -1,6 +1,8 @@
 # Global AnomalyManager
 extends Node
 
+const WIN = preload("uid://1oeiarh1e408")
+
 const ANOMALY_CHANCE = 1.0#0.6
 
 var anomalies: Array[AnomalyEvent]
@@ -13,6 +15,7 @@ var did_purchase := false
 var anomaly_present := false
 
 var progress := 0
+const win_state := 8
 
 func _ready():
 	load_anomalies()
@@ -35,6 +38,11 @@ func start_random():
 	else:
 		progress+=1
 		get_tree().get_first_node_in_group("clock").set_percent_open_to_close(progress/8.0)
+
+	# Check win
+	if progress > 1:# win_state:
+		get_tree().call_deferred("change_scene_to_packed", WIN)
+		return
 
 	# Reset world
 	if current_anomaly != null:
